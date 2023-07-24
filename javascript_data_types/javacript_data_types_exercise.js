6ab1296accaf5d307de0f78d64497db17b8b89c8
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-07-24 19:17:35 +0300

    added data types exercises

--- javascript_data_types/javacript_data_types_exercise.js
+++ javascript_data_types/javacript_data_types_exercise.js
@@ -11,6 +11,23 @@ const myArray = [myBoolean, myString];
 
 const myObject = {firstProperty: myArray, sumProperty: first_number + secondNumber};
 
-console.log(myObject)
-console.log(myObject.sumProperty)
-console.log(myObject.firstProperty[1])
+console.log(myObject);
+console.log(myObject.sumProperty);
+console.log(myObject.firstProperty[1]);
+
+const variable1 = 20;
+const variable2 = "40";
+const resultVariable = variable1 + variable2;
+console.log(resultVariable);
+
+const objectVariable = {
+    property1: 'I am property one',
+    property2: 'I am property two',
+    property3: 'I am property three',
+}
+
+console.log(objectVariable.property2);
+
+const myArray2 = [20, 30, 40];
+
+console.log(myArray[2])
